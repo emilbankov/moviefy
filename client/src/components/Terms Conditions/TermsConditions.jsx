5cc4b166faca8979fb3da081dc569775f4daa3c1
emilbankov <emilbankov17d@gmail.com>
2024-11-23 18:33:21 +0200

    Added Terms & Conditions page

---
+++ client/src/components/Terms Conditions/TermsConditions.jsx
@@ -0,0 +1,113 @@
+export default function TermsConditions() {
+    return (
+        <>
+            <section className="inner-banner bg-holder bg-overlay-secondary-3" style={{ backgroundImage: "url(images/bg/inner-banner-bg.jpg)" }}>
+                <div className="container position-relative">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="header-inner-title">
+                                <h2 className="title">Terms and Conditions</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="space-ptb">
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-md-12">
+                            <div className="mb-4">
+                                <h4 className="mb-3 fw-600">Description of Service</h4>
+                                <p>
+                                    The best way is to develop and follow a plan. Start with your
+                                    goals in mind and then work backwards to develop the plan. What
+                                    steps are required to get you to the goals? Make the plan as
+                                    detailed as possible. Try to visualize and then plan for, every
+                                    possible setback.{" "}
+                                </p>
+                            </div>
+                            <div className="mb-4">
+                                <h4 className="mb-3 fw-600">Your Registration Obligations</h4>
+                                <p>
+                                    Along with your plans, you should consider developing an action
+                                    orientation that will keep you motivated to move forward at all
+                                    times. This requires a little self-discipline, but is a crucial
+                                    component to achievement of any kind. Before starting any new
+                                    activity, ask yourself if that activity will move you closer to
+                                    your goals. If the answer is no, you may want to reconsider doing
+                                    it at that time.
+                                </p>
+                            </div>
+                            <div className="mb-4">
+                                <h4 className="mb-3 fw-600">
+                                    User Account, Password, and Security
+                                </h4>
+                                <p>
+                                    Commitment is something that comes from understanding that
+                                    everything has its price and then having the willingness to pay
+                                    that price.
+                                </p>
+                            </div>
+                            <div className="mb-4">
+                                <h4 className="mb-3 fw-600">User Conduct</h4>
+                                <p>
+                                    Benjamin Franklin, inventor, statesman, writer, publisher and
+                                    economist relates in his autobiography that early in his life he
+                                    decided to focus on arriving at moral perfection. He made a list
+                                    of 13 virtues, assigning a page to each. Under each virtue he
+                                    wrote a summary that gave it fuller meaning. Then he practiced
+                                    each one for a certain length of time. To make these virtues a
+                                    habit,{" "}
+                                </p>
+                                <ul className="list list-unstyled mb-3">
+                                    <li>
+                                        <i className="fas fa-check" /> The truth about success{" "}
+                                    </li>
+                                    <li>
+                                        <i className="fas fa-check" /> If success is a process with a
+                                        number of defined steps,{" "}
+                                    </li>
+                                    <li>
+                                        <i className="fas fa-check" /> Then it is just like any other
+                                        process.{" "}
+                                    </li>
+                                    <li>
+                                        <i className="fas fa-check" /> The first action is always in
+                                        making the decision to proceed.
+                                    </li>
+                                    <li>
+                                        <i className="fas fa-check" /> This is a fundamental step, which
+                                        most people overlook.
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="mb-4">
+                                <h4 className="mb-3 fw-600">International Use</h4>
+                                <p>
+                                    Acres of Diamonds… you’ve read the famous story, or at least had
+                                    it related to you. A farmer hears tales of diamonds and begins
+                                    dreaming of vast riches. He sells his farm and hikes off over the
+                                    horizon, never to be heard from again. Rumors say that years later
+                                    he died destitute, never having found the diamonds he spent his
+                                    life seeking. Meanwhile, the man who bought that farm found a
+                                    large and “interesting looking” stone in a 4K Star that ran
+                                    through the property. He put the stone on his mantle where a
+                                    visitor recognized the large stone as a rough diamond. It turned
+                                    out to be the Hope Diamond.
+                                </p>
+                            </div>
+                            <div className="mb-4">
+                                <a href="#" className="btn btn-primary mt-3">
+                                    Accept
+                                </a>
+                                <a href="#" className="btn btn-white mt-3">
+                                    Close
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
