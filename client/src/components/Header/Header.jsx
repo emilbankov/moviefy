cddd20cf7c5f8eeff2317b734aa7d6b693fd3c23
emilbankov <emilbankov17d@gmail.com>
2024-11-24 17:57:11 +0200

    Updated Header component

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { Link } from 'react-router-dom'
 
 function Header() {
     return (
@@ -6,86 +6,64 @@ function Header() {
             <header className="header header-style-01 header-sticky">
                 <nav className="navbar navbar-static-top navbar-expand-lg">
                     <div className="container main-header position-relative">
-                        <button type="button" className="navbar-toggler" data-bs-toggle="collapse"
-                            data-bs-target=".navbar-collapse"><i className="fas fa-align-left"></i></button>
-                        <a className="navbar-brand" href="index.html">
-                            <img className="img-fluid" src="images/logo.png" alt="logo" />
-                        </a>
+                        <button type="button" className="navbar-toggler" data-bs-toggle="collapse" data-bs-target=".navbar-collapse"><i className="fas fa-align-left"></i></button>
+                        <Link className="navbar-brand" to="/"><img className="img-fluid" src="images/logo.png" alt="logo" /></Link>
                         <div className="navbar-collapse collapse justify-content-end justify-content-lg-center">
                             <ul className="nav navbar-nav">
                                 <li className="nav-item dropdown">
-                                    <a className="nav-link" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
-                                        aria-haspopup="true" aria-expanded="false">Home<i
-                                            className="fas fa-chevron-down fa-xs"></i></a>
+                                    <a className="nav-link" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Home<i className="fas fa-chevron-down fa-xs"></i></a>
                                     <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                        <li><a className="dropdown-item" href="index.html"><span>Home</span></a>
-                                        </li>
-                                        <li><a className="dropdown-item" href="most-popular.html"><span>Most Popular</span></a></li>
-                                        <li><a className="dropdown-item" href="landing-page.html"><span>Landing Page</span></a></li>
+                                        <li><Link className="dropdown-item" to="/"><span>Home</span></Link></li>
+                                        <li><Link className="dropdown-item" to="/most-popular"><span>Most Popular</span></Link></li>
+                                        <li><Link className="dropdown-item" to="/landing-page"><span>Landing Page</span></Link></li>
                                     </ul>
                                 </li>
                                 <li className="nav-item dropdown">
-                                    <a className="nav-link" href="javascript:void(0)" data-bs-toggle="dropdown">Movie<i
-                                        className="fas fa-chevron-down fa-xs"></i></a>
+                                    <a className="nav-link" href="javascript:void(0)" data-bs-toggle="dropdown">Movie<i className="fas fa-chevron-down fa-xs"></i></a>
                                     <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                        <li><a className="dropdown-item" href="movies.html"><span>Movie</span></a></li>
-                                        <li><a className="dropdown-item" href="movies-details.html"><span>Movie Single</span></a></li>
+                                        <li><Link className="dropdown-item" to="/movies"><span>Movie</span></Link></li>
+                                        <li><Link className="dropdown-item" to="/movie-details"><span>Movie Single</span></Link></li>
                                     </ul>
                                 </li>
                                 <li className=" nav-item dropdown">
-                                    <a className="nav-link" href="javascript:void(0)" data-bs-toggle="dropdown">Tv Shows<i
-                                        className="fas fa-chevron-down fa-xs"></i></a>
+                                    <a className="nav-link" href="javascript:void(0)" data-bs-toggle="dropdown">Tv Shows<i className="fas fa-chevron-down fa-xs"></i></a>
                                     <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                        <li><a className="dropdown-item" href="series.html"><span>Tv Show</span></a></li>
-                                        <li><a className="dropdown-item" href="series-details.html"><span>Tv Show Single</span></a>
-                                        </li>
+                                        <li><Link className="dropdown-item" to="/series"><span>Tv Show</span></Link></li>
+                                        <li><Link className="dropdown-item" to="/series-details"><span>Tv Show Single</span></Link></li>
                                     </ul>
                                 </li>
                                 <li className="nav-item dropdown">
-                                    <a className="nav-link" href="javascript:void(0)" data-bs-toggle="dropdown">Web Series<i
-                                        className="fas fa-chevron-down fa-xs"></i></a>
+                                    <a className="nav-link" href="javascript:void(0)" data-bs-toggle="dropdown">Web Series<i className="fas fa-chevron-down fa-xs"></i></a>
                                     <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                        <li><a className="dropdown-item" href="web-series.html"><span>Web Series</span></a></li>
-                                        <li><a className="dropdown-item" href="web-series-single.html"><span>Web Series
-                                            Single</span></a></li>
+                                        <li><Link className="dropdown-item" to="/web-series"><span>Web Series</span></Link></li>
+                                        <li><Link className="dropdown-item" to="/web-series-single"><span>Web Series Single</span></Link></li>
                                     </ul>
                                 </li>
                                 <li className="dropdown nav-item mega-menu ">
-                                    <a className="nav-link" href="javascript:void(0)" data-bs-toggle="dropdown">Pages<i
-                                        className="fas fa-chevron-down fa-xs"></i></a>
+                                    <a className="nav-link" href="javascript:void(0)" data-bs-toggle="dropdown">Pages<i className="fas fa-chevron-down fa-xs"></i></a>
                                     <ul className="dropdown-menu dropdown-menu-md megamenu">
                                         <li>
                                             <div className="row">
                                                 <div className="col-sm-6">
                                                     <span className="nav-title">Pages</span>
                                                     <ul className="list-unstyled">
-                                                        <li><a className="dropdown-item" href="about.html"><span>About
-                                                            us</span></a></li>
-                                                        <li><a className="dropdown-item" href="faqs.html"><span>Faqs</span></a></li>
-                                                        <li><a className="dropdown-item"
-                                                            href="pricing.html"><span>Pricing</span></a></li>
-                                                        <li><a className="dropdown-item" href="blog.html"><span>Blog</span></a></li>
-                                                        <li><a className="dropdown-item" href="blog-single.html"><span>Blog
-                                                            Single</span></a></li>
-                                                        <li><a className="dropdown-item" href="contact-us.html"><span>Contact
-                                                            us</span></a></li>
+                                                        <li><Link className="dropdown-item" to="/about"><span>About us</span></Link></li>
+                                                        <li><Link className="dropdown-item" to="/faqs"><span>Faqs</span></Link></li>
+                                                        <li><Link className="dropdown-item" to="/pricing"><span>Pricing</span></Link></li>
+                                                        <li><Link className="dropdown-item" to="/blog"><span>Blog</span></Link></li>
+                                                        <li><Link className="dropdown-item" to="/blog-single"><span>Blog Single</span></Link></li>
+                                                        <li><Link className="dropdown-item" to="/contact"><span>Contact us</span></Link></li>
                                                     </ul>
                                                 </div>
                                                 <div className="col-sm-6 mt-3 mt-sm-0">
                                                     <span className="nav-title">Authentication</span>
                                                     <ul className="list-unstyled">
-                                                        <li><a className="dropdown-item" href="login.html"><span>Login</span></a>
-                                                        </li>
-                                                        <li><a className="dropdown-submenu" href="my-account.html"><span>My
-                                                            Account</span></a></li>
-                                                        <li><a className="dropdown-submenu" href="coming-soon.html"><span>Coming
-                                                            Soon</span></a></li>
-                                                        <li><a className="dropdown-submenu" href="404.html"><span>Error
-                                                            404</span></a></li>
-                                                        <li><a className="dropdown-submenu" href="privacy-policy.html"><span>Privacy
-                                                            Policy</span></a></li>
-                                                        <li><a className="dropdown-submenu"
-                                                            href="terms-and-conditions.html"><span>T&C</span></a></li>
+                                                        <li><Link className="dropdown-item" to="/login-register"><span>Login</span></Link></li>
+                                                        <li><Link className="dropdown-submenu" to="/account"><span>My Account</span></Link></li>
+                                                        <li><Link className="dropdown-submenu" to="/coming-soon"><span>Coming Soon</span></Link></li>
+                                                        <li><Link className="dropdown-submenu" to="/error"><span>Error 404</span></Link></li>
+                                                        <li><Link className="dropdown-submenu" to="/privacy-policy"><span>Privacy Policy</span></Link></li>
+                                                        <li><Link className="dropdown-submenu" to="/terms-and-conditions"><span>T&C</span></Link></li>
                                                     </ul>
                                                 </div>
                                             </div>
@@ -99,9 +77,10 @@ function Header() {
                                 <li className="search"><a href="#search"><i className="fa fa-search"></i></a></li>
                                 <li><a href="my-account.html"><i className="fa-regular fa-user"></i></a></li>
                                 <li className="dropdown">
-                                    <a href="javascript:void(0)" className="notifications dropdown-toggle" id="dropdownMenuButton2"
-                                        data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i
-                                            className="fa-regular fa-bell"></i><span className="count">3</span></a>
+                                    <a href="javascript:void(0)" className="notifications dropdown-toggle" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <i className="fa-regular fa-bell"></i>
+                                        <span className="count">3</span>
+                                    </a>
                                     <div className="dropdown-menu mt-0 notifications-menu" aria-labelledby="dropdownMenuButton2">
                                         <h6 className="notifications-title">Notifications</h6>
                                         <div className="notifications-list">
