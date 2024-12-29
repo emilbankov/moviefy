0c1377bac0c04b1259ad5efdcd11027592b970a1
emilbankov <emilbankov17d@gmail.com>
2024-12-29 21:47:57 +0200

    Added more requests to movies service

--- client/src/services/moviesService.js
+++ client/src/services/moviesService.js
@@ -1,10 +1,14 @@
 import { get, post, put, del } from '../lib/request.js';
 
 const baseUrl = 'https://moviefy-vwnq.onrender.com';
+const collection = 'Home Alone Collection'
+const collections = 'The Fast and the Furious Collection, Die Hard Collection, The Avengers Collection, Harry Potter Collection, The Dark Knight Collection, Star Wars Collection, Spider-Man Collection, Jurassic Park Collection'
 
-export const getBannerMovies = async () => await get(`${baseUrl}/movies/collection?name=home alone collection`);
+export const getBannerMovies = async () => await get(`${baseUrl}/movies/collection?name=${collection}`);
+export const getLatestMovies = async () => await get(`${baseUrl}/movies/latest`);
 export const getTrendingMovies = async () => await get(`${baseUrl}/movies/trending`);
 export const getPopularMovies = async () => await get(`${baseUrl}/movies/popular`);
+export const getPopularCollections = async () => await get(`${baseUrl}/movies/collections?names=${collections}`);
 export const getMovieDetails = async (movieId) => await get(`${baseUrl}/movies/${movieId}`);
 export const getTrendingSeries = async () => await get(`${baseUrl}/series/trending`);
 export const getPopularSeries = async () => await get(`${baseUrl}/series/popular`);
