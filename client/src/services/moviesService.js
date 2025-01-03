55bd0b0ef4dc8e460241602104335655e37491c6
emilbankov <emilbankov17d@gmail.com>
2025-01-03 22:38:03 +0200

    Separated movies and series services

--- client/src/services/moviesService.js
+++ client/src/services/moviesService.js
@@ -2,7 +2,8 @@ import { get, post, put, del } from '../lib/request.js';
 
 const baseUrl = 'https://moviefy-vwnq.onrender.com';
 const collection = 'Home Alone Collection'
-const collections = 'The Fast and the Furious Collection, Die Hard Collection, The Avengers Collection, Harry Potter Collection, The Dark Knight Collection, Star Wars Collection, Spider-Man Collection, Jurassic Park Collection'
+const collections = 'The Fast and the Furious Collection, Die Hard Collection, The Avengers Collection, Harry Potter Collection, The Dark Knight Collection, Star Wars Collection, Mission: Impossible Collection, Jurassic Park Collection, Venom Collection, Top Gun Collection'
+const seriesCollection = 'The Penguin, Squid Game, Money Heist, Hawaii Five-0';
 
 export const getBannerMovies = async () => await get(`${baseUrl}/movies/collection?name=${collection}`);
 export const getLatestMovies = async () => await get(`${baseUrl}/movies/latest?size=12`);
@@ -10,7 +11,3 @@ export const getTrendingMovies = async () => await get(`${baseUrl}/movies/trendi
 export const getPopularMovies = async () => await get(`${baseUrl}/movies/popular`);
 export const getPopularCollections = async () => await get(`${baseUrl}/movies/collections?names=${collections}`);
 export const getMovieDetails = async (movieId) => await get(`${baseUrl}/movies/${movieId}`);
-export const getLatestSeries = async () => await get(`${baseUrl}/series/latest?size=12`);
-export const getTrendingSeries = async () => await get(`${baseUrl}/series/trending`);
-export const getPopularSeries = async () => await get(`${baseUrl}/series/popular`);
-export const getSeriesDetails = async (seriesId) => await get(`${baseUrl}/series/${seriesId}`);
---
+++ client/src/services/seriesService.js
@@ -0,0 +1,10 @@
+import { get, post, put, del } from '../lib/request.js';
+
+const baseUrl = 'https://moviefy-vwnq.onrender.com';
+const collection = 'Peaky Blinders, Squid Game, Money Heist, Hawaii Five-0';
+
+export const getBannerSeries = async () => await get(`${baseUrl}/series/collection?names=${collection}`);
+export const getLatestSeries = async () => await get(`${baseUrl}/series/latest?size=12`);
+export const getTrendingSeries = async () => await get(`${baseUrl}/series/trending`);
+export const getPopularSeries = async () => await get(`${baseUrl}/series/popular`);
+export const getSeriesDetails = async (seriesId) => await get(`${baseUrl}/series/${seriesId}`);
