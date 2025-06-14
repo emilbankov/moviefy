512ea2a1298bf56bba900f906763035c911da542
emilbankov <emilbankov17d@gmail.com>
2025-06-14 11:43:24 -0400

    Fixed url

--- client/src/services/moviesService.js
+++ client/src/services/moviesService.js
@@ -1,6 +1,6 @@
 import { get, post, put, del } from '../lib/request.js';
 
-const baseUrl = 'https://direct-wanda-boklucite2-df93158d.koyeb.app/';
+const baseUrl = 'https://direct-wanda-boklucite2-df93158d.koyeb.app';
 const collection = 'Home Alone Collection'
 const collections = 'The Fast and the Furious Collection, Die Hard Collection, The Avengers Collection, Harry Potter Collection, The Dark Knight Collection, Star Wars Collection, The Godfather Collection, Jurassic Park Collection, Venom Collection, Top Gun Collection'
 const seriesCollection = 'The Penguin, Squid Game, Money Heist, Hawaii Five-0';
