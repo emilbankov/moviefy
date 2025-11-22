417d200a858867cc687228c1030c817d34a781af
emilbankov <emilbankov17d@gmail.com>
2025-11-22 16:25:43 +0200

    Add getTopRatedSeries API method
    Introduces a new function to fetch top-rated series from the API, similar to existing methods for latest, trending, and popular series.

--- client/src/services/seriesService.js
+++ client/src/services/seriesService.js
@@ -7,5 +7,6 @@ export const getBannerSeries = async () => await get(`${baseUrl}/series/collecti
 export const getLatestSeries = async (types, page, size, genres) => await get(`${baseUrl}/series/latest?types=${types}&page=${page}&size=${size}&genres=${genres || ''}`);
 export const getTrendingSeries = async (types, page, size, genres) => await get(`${baseUrl}/series/trending?types=${types}&page=${page}&size=${size}&genres=${genres || ''}`);
 export const getPopularSeries = async (types, page, size, genres) => await get(`${baseUrl}/series/popular?types=${types}&page=${page}&size=${size}&genres=${genres || ''}`);
+export const getTopRatedSeries = async (types, page, size, genres) => await get(`${baseUrl}/series/top_rated?types=${types}&page=${page}&size=${size}&genres=${genres || ''}`);
 export const getSeriesDetails = async (seriesId) => await get(`${baseUrl}/series/${seriesId}`);
 export const getEpisodes = async (seasonId) => await get(`${baseUrl}/series/season/${seasonId}`);
