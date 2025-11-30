903e6c06d0f900df46575668bd2ea7dac84d7505
emilbankov <emilbankov17d@gmail.com>
2025-11-30 21:16:07 +0200

    Add getProductionCompanies service method
    Introduces getProductionCompanies to fetch media for a given production company, similar to existing actor and crew media fetch methods.

--- client/src/services/castService.js
+++ client/src/services/castService.js
@@ -3,4 +3,5 @@ import { get, post, put, del } from '../lib/request.js';
 const baseUrl = 'https://moviefy-vwnq.onrender.com';
 
 export const getActorsMedia = async (actorId, page = 1, size = 30, mediaType) => await get(`${baseUrl}/cast/${actorId}/media?page=${page}&size=${size}&media_type=${mediaType}`);
+export const getProductionCompanies = async (prodId, page = 1, size = 30, mediaType) => await get(`${baseUrl}/prod/${prodId}/media?page=${page}&size=${size}&media_type=${mediaType}`);
 export const getCrewMedia = async (actorId, page = 1, size = 30, mediaType) => await get(`${baseUrl}/crew/${actorId}/media?page=${page}&size=${size}&media_type=${mediaType}`);
