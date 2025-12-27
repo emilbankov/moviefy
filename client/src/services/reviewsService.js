4c2b36ab5ac28ef3815ee870ed357339855de9b0
emilbankov <emilbankov17d@gmail.com>
2025-12-28 00:53:28 +0200

    Remove trailing slash from baseUrl in service files
    Updated the baseUrl in castService, moviesService, reviewsService, and seriesService to remove the trailing slash. This ensures consistent URL formatting and prevents potential issues with endpoint concatenation.

--- client/src/services/reviewsService.js
+++ client/src/services/reviewsService.js
@@ -1,6 +1,6 @@
 import { get, post, put, del } from '../lib/request.js';
 
 // const baseUrl = 'https://moviefy-vwnq.onrender.com';
-const baseUrl = 'https://p01--moviefy--kc4tkpjph9bk.code.run/';
+const baseUrl = 'https://p01--moviefy--kc4tkpjph9bk.code.run';
 
 export const getReviews = async (mediaId, mediaType, page = 1) => await get(`${baseUrl}/${mediaType}/${mediaId}/reviews?page=${page}`);
