62d79a961d7c9d594839347f6b70a98790259dca
emilbankov <emilbankov17d@gmail.com>
2026-01-13 16:02:51 +0200

    Changed cards again

--- client/src/components/Account/Account.jsx
+++ client/src/components/Account/Account.jsx
@@ -327,7 +327,7 @@ export default function Account() {
                                                             <>
                                                                 <div className="row">
                                                                     {profile.data.favorite_movies.slice(0, 8).map((movie, index) => (
-                                                                        <div key={movie.id || index} className="col-lg-3 col-md-6 col-sm-6 mb-3">
+                                                                        <div key={movie.id || index} className="col-xl-3 col-lg-3 col-md-6 col-sm-6 mb-3">
                                                                             <Link to={`/movie/details/${movie.api_id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
                                                                                 <div className="movies-categories br-20">
                                                                                     <div className="movies-img">
@@ -425,7 +425,7 @@ export default function Account() {
                                                             <>
                                                                 <div className="row">
                                                                     {profile.data.favorite_tv_series.slice(0, 8).map((series, index) => (
-                                                                        <div key={series.id || index} className="col-lg-3 col-md-6 col-sm-6 mb-3">
+                                                                        <div key={series.id || index} className="col-xl-3 col-lg-3 col-md-6 col-sm-6 mb-3">
                                                                             <Link to={`/series/details/${series.api_id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
                                                                                 <div className="movies-categories br-20">
                                                                                     <div className="movies-img">
